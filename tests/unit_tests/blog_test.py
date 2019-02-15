a8cd7d25ba08ab0ce024e5ccd2931998749dd7ad
mrychlicka <rychlickam@gmail.com>
2019-02-15 10:14:33 +0100

    unittests added

---
+++ tests/unit_tests/blog_test.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from blog import Blog
+
+
+class BlogTest(TestCase):
+
+	def test_create_blog(self):
+		b = Blog('Test Title', 'Test Author')
+
+		self.assertEqual('Test Title', b.title)
+		self.assertEqual('Test Author', b.author)
+		self.assertListEqual([], b.posts)
+
+	def test_repr(self):
+		b = Blog('Test Title', 'Test Author')
+		b2 = Blog('My day', 'Susan')
+
+		self.assertEqual(b.__repr__(), 'Test Title by Test Author (0 post)')
+		self.assertEqual(b2.__repr__(), 'My day by Susan (0 post)')
+
+	def test_repr_multiple_posts(self):
+		b = Blog('Meet and eat', 'Tom')
+		b.posts = ['food post']
+		b1 = Blog('I have a dog', 'Mary P')
+		b1.posts = ['some post', 'dog post']
+
+		self.assertEqual(b.__repr__(), 'Meet and eat by Tom (1 post)')
+		self.assertEqual(b1.__repr__(), 'I have a dog by Mary P (2 posts)')
---
+++ tests/unit_tests/post_test.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from post import Post
+
+
+class PostTest(TestCase):
+
+	def test_create_post(self):
+		"""
+		Check if properties of Post class matches.
+		"""
+		p = Post('Test', 'Test Content')
+
+		self.assertEqual('Test', p.title)
+		self.assertEqual('Test Content', p.content)
+
+	def test_json(self):
+		p = Post('Test', 'Test Content')
+		expected = {'title': 'Test', 'content': 'Test Content'}
+
+		self.assertDictEqual(expected, p.json())
